a26a73c60f693a6d3b0a05023cb2b11a24aef857
B4lck <nikolaisaldernbalck@gmail.com>
2023-04-19 12:15:29 +0200

    Better safe than sorry

--- Pokemon/Assets/Scripts/Attack.cs
+++ Pokemon/Assets/Scripts/Attack.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "New Pokemon", menuName = "Attack")]
+[CreateAssetMenu(fileName = "New Attack", menuName = "Attack")]
 public class Attack : ScriptableObject
 {
     public string Name;
